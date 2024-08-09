aa3696838b39a670c7f65e32fd37e8892a42b317
James Nesbitt <jnesbitt@mirantis.com>
2024-08-09 20:25:50 +0300

    separate host plugin factories to their own files
    Signed-off-by: James Nesbitt <jnesbitt@mirantis.com>

--- pkg/product/mcr/hostplugin.go
+++ pkg/product/mcr/hostplugin.go
@@ -37,36 +37,6 @@ func init() {
 	host.RegisterHostPluginFactory(HostRoleMCR, &hostPluginFactory{})
 }
 
-type hostPluginFactory struct {
-	ps []*hostPlugin
-}
-
-// HostPlugin build a new host plugin.
-func (pf *hostPluginFactory) HostPlugin(_ context.Context, h *host.Host) host.HostPlugin {
-	p := &hostPlugin{
-		h: h,
-	}
-	pf.ps = append(pf.ps, p)
-
-	return p
-}
-
-// Decoder provide a Host Plugin decoder function
-//
-// The decoder function is ugly, but it is meant to to take a
-// yaml/json .HostPluginDecode() function, and turn it into a plugin.
-func (pf *hostPluginFactory) HostPluginDecode(_ context.Context, h *host.Host, d func(interface{}) error) (host.HostPlugin, error) {
-	p := &hostPlugin{h: h}
-
-	if err := d(p); err != nil {
-		return p, err
-	}
-
-	pf.ps = append(pf.ps, p)
-
-	return p, nil
-}
-
 // Get the MCR plugin from a Host.
 func HostGetMCR(h *host.Host) *hostPlugin {
 	hgmcr := h.MatchPlugin(HostRoleMCR)
--- pkg/product/mkex/hostplugin.go
+++ pkg/product/mkex/hostplugin.go
@@ -2,17 +2,12 @@ package mkex
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
 	"io"
-	"log/slog"
 
 	"github.com/Mirantis/launchpad/pkg/host"
 	"github.com/Mirantis/launchpad/pkg/host/exec"
 	dockerimplementation "github.com/Mirantis/launchpad/pkg/implementation/docker"
 	dockerhost "github.com/Mirantis/launchpad/pkg/implementation/docker/host"
-	"github.com/Mirantis/launchpad/pkg/product/mkex/implementation"
-	"github.com/spf13/cobra"
 )
 
 /**
@@ -28,75 +23,6 @@ var (
 	// ManagerHostRoles the Host roles accepted for managers.
 	ManagerHostRoles = []string{"manager"}
 )
-
-func init() {
-	host.RegisterHostPluginFactory(HostRoleMKEX, &hostPluginFactory{})
-}
-
-type hostPluginFactory struct {
-	ps []*hostPlugin
-}
-
-// HostPlugin build a new host plugin.
-func (pf *hostPluginFactory) HostPlugin(_ context.Context, h *host.Host) host.HostPlugin {
-	p := &hostPlugin{
-		h: h,
-	}
-	pf.ps = append(pf.ps, p)
-
-	return p
-}
-
-// Decoder provide a Host Plugin decoder function
-//
-// The decoder function is ugly, but it is meant to to take a
-// yaml/json .HostPluginDecode() function, and turn it into a plugin.
-func (pf *hostPluginFactory) HostPluginDecode(_ context.Context, h *host.Host, d func(interface{}) error) (host.HostPlugin, error) {
-	p := &hostPlugin{h: h}
-	pf.ps = append(pf.ps, p)
-
-	err := d(p)
-
-	return p, err
-}
-
-func (pf *hostPluginFactory) CliBuild(cmd *cobra.Command, c *host.HostsComponent) error {
-	var exec_hn string
-	exec := &cobra.Command{
-		GroupID: c.Name(),
-		Use:     fmt.Sprintf("%s:rpm-ostree:status", c.Name()),
-		Short:   "Retrieve host rpm-ostree status",
-		Long:    ``,
-		RunE: func(cmd *cobra.Command, args []string) error {
-			ctx := cmd.Context()
-
-			h := c.Hosts().Get(exec_hn)
-			if h == nil {
-				for id, h := range c.Hosts() {
-					slog.Info("host", slog.String("id", id), slog.Any("host", h))
-				}
-				return fmt.Errorf("%s: host not found", exec_hn)
-			}
-
-			he := exec.HostGetExecutor(h)
-			rosi := implementation.NewRpmOSTreeExecClient(he, exec.ExecOptions{})
-
-			s, serr := rosi.Status(ctx)
-			if serr != nil {
-				return fmt.Errorf("%s: rpm-ostree status error: %s", h.Id(), serr.Error())
-			}
-
-			so, _ := json.Marshal(s)
-			fmt.Println(so)
-
-			return nil
-		},
-	}
-	exec.Flags().StringVar(&exec_hn, "host", "", "host to execute on")
-	cmd.AddCommand(exec)
-
-	return nil
-}
 
 // Get the MKEX plugin from a Host.
 func HostGetMKEX(h *host.Host) *hostPlugin {

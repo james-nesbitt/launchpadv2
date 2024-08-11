40994b3c8328f7fefdba07389adda0a437422aff
James Nesbitt <jnesbitt@mirantis.com>
2024-08-11 11:53:24 +0300

    2.1 config
    Signed-off-by: James Nesbitt <jnesbitt@mirantis.com>

---
+++ pkg/config/v2_1/decode.go
@@ -0,0 +1,36 @@
+package v21
+
+import (
+	"strings"
+
+	"github.com/Mirantis/launchpad/pkg/component"
+	"github.com/Mirantis/launchpad/pkg/config"
+	"github.com/Mirantis/launchpad/pkg/project"
+)
+
+const (
+	ID = "launchpad.mirantis.com/v2.1"
+)
+
+func init() {
+	config.RegisterSpecDecoder(ID, Decode)
+}
+
+// Decode a project from the spec.
+func Decode(cl *project.Project, d func(interface{}) error) error {
+	var cs Spec
+
+	if err := d(&cs); err != nil {
+		return err
+	}
+
+	if cl.Components == nil {
+		cl.Components = component.Components{}
+	}
+
+	for id, cp := range cs.Products.products() {
+		cl.Components[strings.ToUpper(id)] = cp
+	}
+
+	return nil
+}

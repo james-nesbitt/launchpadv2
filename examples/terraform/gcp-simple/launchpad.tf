46d54732558c4be7aa830aed3b2bf6773ed8892f
James Nesbitt <jnesbitt@mirantis.com>
2026-05-01 09:34:17 +0300

    feat(k0s): modernize k0s component with flexible config and improved roles
    - Use dig.Mapping for K0sConfig to allow arbitrary YAML structures
    - Add support for 'controller+worker', 'single' roles
    - Correctly handle --enable-worker and --single flags during install
    - Improve 'project:status' by implementing real host discovery
    - Ensure sensible defaults for storage type and API ports in BuildHostConfig

--- examples/terraform/gcp-simple/launchpad.tf
+++ examples/terraform/gcp-simple/launchpad.tf
@@ -13,7 +13,16 @@ spec:
             address: ${google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip}
             user: ${var.ssh_user}
             keyPath: ${abspath(local_sensitive_file.private_key.filename)}
+        k0s:
+          role: controller+worker
     k0s:
       version: 1.30.2+k0s.0
+      config:
+        spec:
+          api:
+            sans:
+              - ${google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip}
+          storage:
+            type: etcd
 EOT
 }
